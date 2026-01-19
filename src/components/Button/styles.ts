40ebf47e672fc57dbc2c41d1325a7a2655c4055f
Sabino <i.joseafs@gmail.com>
2026-01-19 10:49:49 -0300

    poc: style fix

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,6 +1,7 @@
 import { styled } from "@mui/material/styles";
 import MuiButton from "@mui/material/Button";
+import { ComponentType } from "react";
 
-export const StyledButton = styled(MuiButton)`
+export const StyledButton: ComponentType<any> = styled(MuiButton)`
   text-transform: none;
 `;
